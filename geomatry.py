3c6cdf04c38dd03e94aec796950904f2cb2c7327
YeLinDev <yelinhtet.mm@protonmail.com>
2022-01-22 09:05:08 +0630

    Create geomatry.py

---
+++ geomatry.py
@@ -0,0 +1,7 @@
+square = float(input("Type in the length of your square: "))
+print(f"The area of the square is {square ** 2}.")
+rectangle_w = float(input("Type in the length of your rectangle: "))
+rectangle_l = float(input("Type in the width of your rectangle: "))
+print(f"The area of the rectangle is {rectangle_w * rectangle_l}.")
+radius = float(input("Type in the radius of your circle: "))
+print(f"The area of the circle is { 3.14*radius**2 }.")
